c01547d16737655a98bbac27a6284e83e16fa1bd
mendiadi <adim333@gmail.com>
2022-08-14 02:30:09 +0300

    fix syntax

--- test_main.py
+++ test_main.py
@@ -1,7 +1,8 @@
 from functions import *
 from selenium.webdriver import Chrome
 import pytest
-import  logging
+import logging
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -21,14 +22,16 @@ def test_drag_and_drop(init_driver):
 def test_alert(init_driver):
     actual, excepted = alert(driver=init_driver)
     LOGGER.info(f"actual results: {actual}, excepted result: {excepted}")
-    for index,msg in enumerate(actual):
+    for index, msg in enumerate(actual):
         assert msg == excepted[index]
+
 
 def test_selection_register(init_driver):
     actual, excepted = selection_register(driver=init_driver)
     LOGGER.info(f"actual results: {actual}, excepted result: {excepted}")
     for msg in excepted:
         assert msg in actual
+
 
 def test_iframe(init_driver):
     actual, excepted = iframe(driver=init_driver)
